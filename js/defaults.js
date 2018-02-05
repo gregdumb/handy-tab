a1063b267f89a83cac18c8c0cdd4539599a3063c
Greg <gregoryjjb@gmail.com>
2018-02-04 16:39:40 -0800

    Updated default bookmarks

--- js/defaults.js
+++ js/defaults.js
@@ -6,23 +6,27 @@ const defaults = {};
 
 defaults.links = [{
     name: "Google",
-    image: "https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg",
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Google-favicon-2015.png",
 	href: "https://google.com"
-}, {
+},{
+	name: "Facebook",
+	image: "https://facebookbrand.com/wp-content/themes/fb-branding/prj-fb-branding/assets/images/fb-art.png",
+	href: "https://facebook.com"
+},{
     name: "Wikipedia",
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png",
 	href: "https://wikipedia.org"
-}, {
-	name: "Facebook",
-	image: "https://cdn.worldvectorlogo.com/logos/facebook-2.svg",
-	href: "https://facebook.com"
+},{
+	name: "Twitter",
+	image: "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1259px-Twitter_Bird.svg.png",
+	href: "https://twitter.com"
 }];
 
 defaults.layout = {
     columns: 4,
     rowHeight: 20,
     linkMargins: 1,
-	linkPadding: 1,
+	linkPadding: 2,
 	linkBorderRadius: 0,
 	pageMargins: 20
 }
